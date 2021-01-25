fa9ddfb4918f37c71751f427007a23b15cb549a2
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-25 15:54:17 +0700

    update: updated readme and added build folder

---
+++ build/precache-manifest.dd6b61856da59c261c911be7366f32b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3791505d22d8ff67b7b7e36eeea0dca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ae68bca9a694dd70ad",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "798805972f207e9085ea",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62ae68bca9a694dd70ad",
+    "url": "/static/js/2.529fa8c2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.529fa8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798805972f207e9085ea",
+    "url": "/static/js/main.6a94cebc.chunk.js"
+  },
+  {
+    "revision": "7388fca79b0e47b7b799",
+    "url": "/static/js/runtime-main.a95b636d.js"
+  }
+]);
